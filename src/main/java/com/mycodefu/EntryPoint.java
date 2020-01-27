4588386aa3ae263942a69a51dca1beaf9c90700b
Luke Thompson <endeavour9@gmail.com>
2020-01-27 18:30:40 +1100

    add a stream of elephant records with realistic fake names and weights

--- src/main/java/com/mycodefu/EntryPoint.java
+++ src/main/java/com/mycodefu/EntryPoint.java
@@ -1,16 +1,29 @@
 package com.mycodefu;
 
-import java.util.Arrays;
+import com.github.javafaker.Faker;
+
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class EntryPoint {
+
     public static void main(String[] args) {
-        List<Elephant> elephants = Arrays.asList(
-                new Elephant("Bobby", 3000),
-                new Elephant("Dobby", 1020),
-                new Elephant("Kobby", 2012)
-        );
+        Faker faker = new Faker();
+        Random random = new Random();
+
+        List<Elephant> elephants = IntStream.rangeClosed(1, 1000).boxed()
+                .map(i -> {
+                    String name = faker.name().fullName();
+                    int weight = 1800 + random.nextInt(4500);
+
+                    return new Elephant(name, weight);
+                })
+                .collect(Collectors.toList());
 
         System.out.println(String.format("Welcome to Java 14! Here are your elephants!\n%s", elephants));
+        System.out.println("The first elephant is:\n"+elephants.get(0));
     }
+
 }
